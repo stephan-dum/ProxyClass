d546229195a874871b2b621e6b19fd8c8ed1f3a7
stephan-dum <stephan.dum@gmail.com>
2018-11-28 20:23:44 +0100

    fixed accessor

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,48 +1,181 @@
 import { read } from '@aboutweb/proxyscope';
 
-const isBound = /^bound .*$/i;
-
 function ProxyClass(...mixins) {
 	function BaseClass(...args) {
-		let instance = {};
+		let isolated = {};
 		let stack = mixins.map((mixin) => new mixin(...args));
 		let proto = Object.getPrototypeOf(this);
 
 		let proxy = read(stack, {
 			get(target, property, receiver) {
-				let desc = (
-					Object.getOwnPropertyDescriptor(proto, property)
-					|| Object.getOwnPropertyDescriptor(proto.prototype || {}, property)
-				);
+				let host = this.findHost(property);
+				let desc;
 
-				if(desc) {
-					let {
-						value,
-						set,
-						get
-					} = desc;
+				if(host) {
+					desc = (
+						Object.getOwnPropertyDescriptor(host, property)
+						|| Object.getOwnPropertyDescriptor(Object.getPrototypeOf(host), property)
+					);
+				} else {
+					host = receiver;
 
-					let result = value || set || get;
-
-					if(typeof result == "function") {
-						return result.bind(receiver);
-					}
+					desc = (
+						Object.getOwnPropertyDescriptor(proto, property)
+						|| Object.getOwnPropertyDescriptor(Object.getPrototypeOf(proto), property)
+					);
 				}
 
-				for(let i = 0, length = stack.length; i < length; i++) {
-					let level = stack[i];
-					let value = level[property];
+				if(desc) {
+					let { value, get } = desc;
 
 					if(value) {
-						if(typeof value == "function" && !isBound.test(value.name)) {
-							return value.bind(level);
+						if(typeof value == "function") {
+							return value.bind(host);
 						}
 
 						return value;
 					}
+
+					if(get) {
+						return get.call(host);
+					}
+				}
+			},
+			set(target, property, value) {
+				return Reflect.set(isolated, property, value);
+			},
+			getPrototypeOf(target) {
+				return proto;
+			}
+		});
+
+		stack.unshift(isolated);
+
+		return proxy;
+	}
+
+	BaseClass.prototype = read(
+		mixins.map((mixin) => mixin.prototype)
+	);
+
+
+	return BaseClass;
+}
+
+ProxyClass.hasInstance = function(...mixins) {
+	let BaseClass = ProxyClass(...mixins);
+ 	let delegators = new Set([BaseClass]);
+
+	mixins.forEach(function(value) {
+		if(value.delegators) {
+			value.delegators.forEach(function(delegator) {
+				delegators.add(delegator);
+			});
+		}
+
+		delegators.add(value.prototype.constructor);
+	});
+
+	delegators.forEach(function(delegator) {
+		var oldInstanceOf = delegator[Symbol.hasInstance];
+
+		Object.defineProperty(
+			delegator,
+			Symbol.hasInstance, {
+				configurable : true,
+				value : function(instance) {
+					if(this == delegator) {
+						/*return (
+							mixins.some((mixin) => mixin.prototype.isPrototypeOf(instance))
+							|| oldInstanceOf.call(delegator, instance)
+						);*/
+
+						return (
+							BaseClass.prototype.isPrototypeOf(instance)
+							|| oldInstanceOf.call(delegator, instance)
+						);
+					} else {
+						return oldInstanceOf.call(this, instance);
+					}
+				}
+			}
+		);
+	});
+
+	BaseClass.delegators = delegators;
+
+	return BaseClass;
+};
+
+/*
+function() {
+	function BaseClass(...args) {
+		//let instance = {};
+		let stack = mixins.map((mixin) => new mixin(...args));
+
+		let superProperty = (property) => {
+			for(let i = 0, length = stack.length; i < length; i++) {
+				let level = stack[i];
+
+				if(property in level) {
+					let value = level[property];
+
+					if(typeof value == "function" && !isBound.test(value.name)) {
+						return value.bind(level);
+					}
+
+					return value;
+				}
+			}
+		}
+
+		let proto = Object.getPrototypeOf(this);
+
+		Object.setPrototypeOf(this, proxyRead(
+			proto,
+			{
+				get(target, property) {
+					console.log("super get", property)
+					return superProperty(property);
+				},
+				getPrototypeOf() {
+					return proto;
+				}
+			}
+		));
+
+
+
+		let proxy = proxyRead(stack, {
+			get(target, property, receiver) {
+
+
+
+
+				let desc = Reflect.getOwnPropertyDescriptor(proto, property);
+
+				if(!desc) {
+					let sub = Reflect.getPrototypeOf(proto);
+
+					if(sub) {
+						desc = Reflect.getOwnPropertyDescriptor(sub, property);
+					}
 				}
 
-				return target[property];
+
+				if(desc) {
+					let { value, get } = desc;
+
+					if(value && typeof value == "function") {
+						return value.bind(receiver);
+					}
+
+					if(get && typeof get) {
+						return get.call(receiver);
+					}
+				}
+
+				return superProperty(property);
 			},
 			set(target, property, value) {
 				return Reflect.set(instance, property, value);
@@ -52,17 +185,28 @@ function ProxyClass(...mixins) {
 			}
 		});
 
-		stack.unshift(instance);
+		//stack.unshift(instance);
 
 		return proxy;
 	}
 
-	BaseClass.prototype = read(
-		mixins.map((mixin) => Object.create(mixin.prototype))
+	BaseClass.prototype = proxyRead(
+		mixins.map((mixin) => Object.create(mixin.prototype)),
+		{
+			get(target, property) {
+				console.log("super get", property)
+				//return superProperty(property);
+			},
+			getPrototypeOf() {
+				console.log("getproto")
+
+			}
+		}
+
 	);
 
 	return BaseClass;
-}
+
 
 ProxyClass.hasInstance = function(...mixins) {
 	let BaseClass = ProxyClass(...mixins);
@@ -102,7 +246,9 @@ ProxyClass.hasInstance = function(...mixins) {
 	BaseClass.delegators = delegators;
 
 	return BaseClass;
-};
+}
+}
+*/
 
 export default ProxyClass;
 //# sourceMappingURL=index.mjs.map
